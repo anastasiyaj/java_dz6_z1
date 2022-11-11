0903860bf9b07f7dfd83c766701929e9d0bbe3c2
Anastasiya Kobeleva <anastasiya.kobeleva.95@gmail.com>
2022-11-11 22:27:47 +0300

    path correction

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-
-            "200, 250, 1",
-            "400, 500, 3",
-            "401, 434, 0"
-    })
-    //@CsvFileSource (files = "src/test/resource/variables.csv")
+//    @CsvSource({
+//
+//          "200, 250, 1",
+//          "400, 500, 3",
+//          "401, 434, 0"
+//    })
+    @CsvFileSource(files = "src/test/resources/variables.csv")
     public void testMinMax(int min, int max, int expected) {
         SQRService service = new SQRService();
         int actual = service.calcSQR(min, max);
